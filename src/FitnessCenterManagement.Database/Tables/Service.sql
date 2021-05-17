738b0cc1635c96b4425922a4db2b09e5fd1c7525
Vadzim Zemchonak <37553515+Zemchonak@users.noreply.github.com>
2021-05-17 23:16:26 +0300

    fixed Service price

--- src/FitnessCenterManagement.Database/Tables/Service.sql
+++ src/FitnessCenterManagement.Database/Tables/Service.sql
@@ -2,7 +2,7 @@
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1, 1),
 	[Name] NVARCHAR(MAX) NOT NULL,
-	[Price] DECIMAL NOT NULL,
+	[Price] DECIMAL(18,2) NOT NULL,
 	[Description] NVARCHAR(MAX) NOT NULL,
 	[SpecializationId] INT NOT NULL,
 )
