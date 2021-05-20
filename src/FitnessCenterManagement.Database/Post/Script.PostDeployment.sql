2420bd1980ddce85c86f531cfaf0ee5c5612cdc9
Vadzim Zemchonak <vdzmch@yandex.ru>
2021-05-20 23:26:13 +0300

    Updated post-deployment script

--- src/FitnessCenterManagement.Database/Post/Script.PostDeployment.sql
+++ src/FitnessCenterManagement.Database/Post/Script.PostDeployment.sql
@@ -14,29 +14,29 @@ values ('15269863-61E7-4877-BE2A-A22602CE742D', 'Trainer', 'TRAINER', 'AJCcr9bpM
 
 --- User
 insert into dbo.[AspNetUsers](Id, Email, EmailConfirmed, PasswordHash, SecurityStamp, PhoneNumber, PhoneNumberConfirmed, TwoFactorEnabled, LockoutEndDateUtc, LockoutEnabled, AccessFailedCount, UserName, Surname, FirstName, LastName, Address, Balance, LanguageId, LockoutEnd, ConcurrencyStamp, NormalizedEmail, NormalizedUserName, AvatarName)
-values ('88527CB8-A870-4757-AED9-DB0BC51624E2', 'trainer@mail.box', 0, 'AJCcr9bpMiB47T4+SMVYdlNTkZeztIazjCxNgKxZwweI+OkJ4Vzembs/YRmPjedcUg==', '9c392224-18f2-4540-9e1d-9a44afa96e88', NULL, 0, 0, NULL, 0, 0, 'trainer@mail.box', 'Trainerova', 'Irina', 'Aleksandrovna', 'Gomel, pr. Rechitsky 25', 0, 1, null, null, 'TRAINER@MAIL.BOX', 'TRAINER@MAIL.BOX', 'defaultAvatar.jpg'),
-('88527CB8-A870-4757-AED9-DB0BC51624E3', 'user@mail.box',     0, 'AJCcr9bpMiB47T4+SMVYdlNTkZeztIazjCxNgKxZwweI+OkJ4Vzembs/YRmPjedcUg==', '9e392224-18f2-4540-9e1d-9a44afa96e87', NULL, 0, 0, NULL, 0, 0, 'user@mail.box',     'Usermanov', 'Ivan', 'Igorevich', 'Gomel, pr. Minsky 95', 0, 1, null, null,		 'USER@MAIL.BOX',    'USER@MAIL.BOX', 'defaultAvatar.jpg'),
-('88527CB8-A870-4757-AED9-DB0BC51624E4', 'manager@mail.box',  0, 'AJCcr9bpMiB47T4+SMVYdlNTkZeztIazjCxNgKxZwweI+OkJ4Vzembs/YRmPjedcUg==', '9e392224-18f2-4540-9e1d-9a44afa96e89', NULL, 0, 0, NULL, 0, 0, 'manager@mail.box',  'Managerov', 'Alexandr', 'Mihailovich', 'Gomel, Pavlova st. 52', 0, 1, null, null, 'MANAGER@MAIL.BOX', 'MANAGER@MAIL.BOX', 'defaultAvatar.jpg'),
-('88527CB8-A870-4757-AED9-DB0BC51624E5', 'director@mail.box', 0, 'AJCcr9bpMiB47T4+SMVYdlNTkZeztIazjCxNgKxZwweI+OkJ4Vzembs/YRmPjedcUg==', '9e392224-18f2-4540-9e1d-9a44afa96e90', NULL, 0, 0, NULL, 0, 0, 'director@mail.box', 'Directorov', 'Yuri', 'Evgenievich', 'Gomel, Mira st. 11', 0, 1, null, null,		 'DIRECTOR@MAIL.BOX','DIRECTOR@MAIL.BOX', 'defaultAvatar.jpg'),
-('88527CB8-A870-4757-AED9-DB0BC51624E6', 'marketer@mail.box', 0, 'AJCcr9bpMiB47T4+SMVYdlNTkZeztIazjCxNgKxZwweI+OkJ4Vzembs/YRmPjedcUg==', '9e392224-18f2-4540-9e1d-9a44afa96e91', NULL, 0, 0, NULL, 0, 0, 'marketer@mail.box', 'Marketologov', 'Stepan', 'Alekseevich', 'Gomel, Kupaly st. 58', 0, 1, null, null, 'MARKETER@MAIL.BOX','MARKETER@MAIL.BOX', 'defaultAvatar.jpg')
+values ('88527CB8-A870-4757-AED9-DB0BC51624E2', 'trainer@mail.box', 0, 'AJCcr9bpMiB47T4+SMVYdlNTkZeztIazjCxNgKxZwweI+OkJ4Vzembs/YRmPjedcUg==', '9c392224-18f2-4540-9e1d-9a44afa96e88', NULL, 0, 0, NULL, 0, 0, 'trainer@mail.box', N'Гимнастикович', N'Ирина', N'Александровна', N'Гомель, пр. Речицкий, 25', 0, 3, null, null, 'TRAINER@MAIL.BOX', 'TRAINER@MAIL.BOX',  'defaultAvatar.jpg'),
+('88527CB8-A870-4757-AED9-DB0BC51624E3', 'user@mail.box',     0, 'AJCcr9bpMiB47T4+SMVYdlNTkZeztIazjCxNgKxZwweI+OkJ4Vzembs/YRmPjedcUg==', '9e392224-18f2-4540-9e1d-9a44afa96e87', NULL, 0, 0, NULL, 0, 0, 'user@mail.box',     N'Клейнов', N'Иван', 'Игоревич', N'Гомель, пр. Минский, 95',					0, 3, null, null, 'USER@MAIL.BOX',    'USER@MAIL.BOX',	   'defaultAvatar.jpg'),
+('88527CB8-A870-4757-AED9-DB0BC51624E4', 'manager@mail.box',  0, 'AJCcr9bpMiB47T4+SMVYdlNTkZeztIazjCxNgKxZwweI+OkJ4Vzembs/YRmPjedcUg==', '9e392224-18f2-4540-9e1d-9a44afa96e89', NULL, 0, 0, NULL, 0, 0, 'manager@mail.box',  N'Менеджов', N'Александр', N'Михайлович', N'Гомель, ул. Павлова, 111',		0, 3, null, null, 'MANAGER@MAIL.BOX', 'MANAGER@MAIL.BOX',  'defaultAvatar.jpg'),
+('88527CB8-A870-4757-AED9-DB0BC51624E5', 'director@mail.box', 0, 'AJCcr9bpMiB47T4+SMVYdlNTkZeztIazjCxNgKxZwweI+OkJ4Vzembs/YRmPjedcUg==', '9e392224-18f2-4540-9e1d-9a44afa96e90', NULL, 0, 0, NULL, 0, 0, 'director@mail.box', N'Диреков', 'Юрий', N'Евгеньевич', N'Гомель, ул. Никитина 15',			0, 3, null, null, 'DIRECTOR@MAIL.BOX','DIRECTOR@MAIL.BOX', 'defaultAvatar.jpg'),
+('88527CB8-A870-4757-AED9-DB0BC51624E6', 'marketer@mail.box', 0, 'AJCcr9bpMiB47T4+SMVYdlNTkZeztIazjCxNgKxZwweI+OkJ4Vzembs/YRmPjedcUg==', '9e392224-18f2-4540-9e1d-9a44afa96e91', NULL, 0, 0, NULL, 0, 0, 'marketer@mail.box', N'Маркитский', N'Степан', N'Алексеевич', N'Гомель, ул. Купалы 48',			0, 3, null, null, 'MARKETER@MAIL.BOX','MARKETER@MAIL.BOX', 'defaultAvatar.jpg')
 
---- UserRoles
-insert into dbo.[AspNetUserRoles](UserId, RoleId)
-values('88527CB8-A870-4757-AED9-DB0BC51624E2', '15269863-61E7-4877-BE2A-A22602CE742D'),
-('88527CB8-A870-4757-AED9-DB0BC51624E3', '15269863-61E7-4877-BE2A-A22602CE742E'),
-('88527CB8-A870-4757-AED9-DB0BC51624E4', '15269863-61E7-4877-BE2A-A22602CE742F'),
-('88527CB8-A870-4757-AED9-DB0BC51624E5', '15269863-61E7-4877-BE2A-A22602CE742G'),
-('88527CB8-A870-4757-AED9-DB0BC51624E6', '15269863-61E7-4877-BE2A-A22602CE742H')
+-- Customers categories
+insert into dbo.[Customer] (UserId, CustomerCategoryId)
+values ('88527CB8-A870-4757-AED9-DB0BC51624E2', null),
+('88527CB8-A870-4757-AED9-DB0BC51624E3', null),
+('88527CB8-A870-4757-AED9-DB0BC51624E4', null),
+('88527CB8-A870-4757-AED9-DB0BC51624E5', null),
+('88527CB8-A870-4757-AED9-DB0BC51624E6', null)
 
 -- Specializations
 insert into dbo.[Specialization](Info)
-values('Test specialization')
+values('Гимнастика')
 
--- Client categories
-insert into dbo.[ClientCategory]([Name], SaleCoefficient)
+-- Customer categories
+insert into dbo.[CustomerCategory]([Name], SaleCoefficient)
 values(N'Пенсионер', 0.9),
 (N'Студент', 0.95)
 
 -- Trainers
-insert into dbo.[Trainer](UserId, SpecializationId)
-values('88527CB8-A870-4757-AED9-DB0BC51624E2', 1)
+insert into dbo.[Trainer](CustomerId, SpecializationId, [Description])
+values(1, 1, N'Лауреат множества спортивных конкурсов в Республике Беларусь и за рубежом, более 15 лет стажа. Настоящий профессионал своего дела')
