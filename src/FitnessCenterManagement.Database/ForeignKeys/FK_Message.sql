99015cc7bea8f4a50b06c7f7d33852fe58ea4690
Vadzim Zemchonak <vdzmch@yandex.ru>
2021-05-20 23:29:27 +0300

    Update database for Message

---
+++ src/FitnessCenterManagement.Database/ForeignKeys/FK_Message.sql
@@ -0,0 +1,6 @@
+﻿ALTER TABLE [dbo].[Message]
+ADD CONSTRAINT FK_Message_UserSender FOREIGN KEY (SenderId)     
+    REFERENCES [dbo].[AspNetUsers] (Id)
+    ALTER TABLE [dbo].[Message]
+ADD CONSTRAINT FK_Message_UserReceiver FOREIGN KEY (ReceiverId)     
+    REFERENCES [dbo].[AspNetUsers] (Id)
