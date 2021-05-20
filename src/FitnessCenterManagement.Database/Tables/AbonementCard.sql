6d6bdd5dd6820443bcfca699755cc2f0bf165adf
Vadzim Zemchonak <vdzmch@yandex.ru>
2021-05-20 22:11:32 +0300

    fixed tables

--- src/FitnessCenterManagement.Database/Tables/AbonementCard.sql
+++ src/FitnessCenterManagement.Database/Tables/AbonementCard.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[AbonementCard]
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1, 1),
-	[CustomerId] NVARCHAR(128) NOT NULL,
+	[CustomerId] INT NOT NULL,
 	[AbonementId] INT NOT NULL,
 	[Visits] INT NOT NULL,
 	[PurchaseDate] DATETIMEOFFSET NOT NULL,
--- src/FitnessCenterManagement.Database/Tables/Trainer.sql
+++ src/FitnessCenterManagement.Database/Tables/Trainer.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[Trainer]
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1, 1),
-	[UserId] NVARCHAR(128) NOT NULL,
+	[CustomerId] INT NOT NULL,
 	[SpecializationId] INT NOT NULL,
 	[Description] NVARCHAR(MAX) NUll,
 )
